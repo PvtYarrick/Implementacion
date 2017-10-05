3cb8d90604f94080ea374ccd18f6f701a037ff00
Guillermo Regueiro <regueiromag@gmail.com>
2017-10-05 18:43:13 +0200

    MovementTest

---
+++ ProyectXeno/Assets/Script/Movement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public Railes rail;
+
+    public PlayMode mode;
+
+    public float speed = 2.5f;
+    public bool isReversed;
+    public bool isLooped;
+    public bool PingPong;
+
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!rail)
+            return;
+
+        if (!isCompleted)
+            Play();
+    }
+    private void Play()
+    {
+        transition += Time.deltaTime * speed;
+
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        transform.position = rail.PositionOnRail(currentSeg, transition, mode);
+
+    }
+
+}
