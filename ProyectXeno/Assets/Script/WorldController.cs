cf412dbfaed5072d9d7e66cd9eb976d34296a825
PC-Guillermo\Guillermo <regueiromag@gmail.com>
2017-10-31 01:34:31 +0100

    Working on it

---
+++ ProyectXeno/Assets/Script/WorldController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    public float worldSpeed;
+
+   
+
+
+    private Vector3 speed;
+    
+
+    void Start()
+    {
+        
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        speed = Vector3.back * worldSpeed;
+
+
+        transform.Translate(speed);
+
+       
+    }
+}
